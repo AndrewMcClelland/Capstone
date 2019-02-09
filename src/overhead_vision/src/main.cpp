dd8f44e3bc759964f9c5d1747443cbc256fdf4de
Nlipski <13ml76@queensu.ca>
2019-02-09 14:13:12 -0500

    Fixing errors

--- src/overhead_vision/src/main.cpp
+++ src/overhead_vision/src/main.cpp
@@ -47,15 +47,23 @@ int main(int, char**)
         GaussianBlur(edges, edges, Size(9,9), 4.0);
         Canny(edges, edges, 0, 30, 3);
         findContours(edges, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+        vector<vector<Point> > contours_poly( contours.size() );
+        vector<Rect> boundRect( contours.size() );
         cout<<"num of objects: "<< contours.size()<<endl;
         for (int i = 0; i < contours.size(); i++) {
             vector<Point> pts = contours.at(i);
+
+            approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
+            boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+
+
             cout << "num of vertecies: "<< pts.size()<<endl;
             //double angle = atan((pts.at(3).y-pts.at(2).y)/(pts.at(3).x-pts.at(2).x));
             cout<<"Object "<<i<<" has points:"<<endl;
             for (int j = 0; j < pts.size(); j++)
                 cout<<"Point "<<j<<" is at: "<< pts.at(j)<<endl;
             drawContours(drawing, contours, i, cv::Scalar(255, 255, 255));
+            rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), cv::Scalar(255, 255, 255), 2, 8, 0 );
         }
         
     
