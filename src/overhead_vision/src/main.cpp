11c1f729d455472e2eb138518e29eab4fcb498ef
Nlipski <13ml76@queensu.ca>
2019-02-09 11:04:31 -0500

    Capture square frames

--- src/overhead_vision/src/main.cpp
+++ src/overhead_vision/src/main.cpp
@@ -5,7 +5,10 @@ using namespace std;
 
 int main(int, char**)
 {
-    VideoCapture cap(0); // open the default camera
+    VideoCapture cap(0);
+    cap.set(CV_CAP_PROP_FRAME_WIDTH, 256); 
+    cap.set(CV_CAP_PROP_FRAME_HEIGHT, 256); 
+    
     if(!cap.isOpened())  // check if we succeeded
         return -1;
 
