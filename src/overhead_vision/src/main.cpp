8e4e580e1da9c64e690f997f133c1b40c57f2efd
Nlipski <13ml76@queensu.ca>
2019-02-13 22:44:18 -0500

    detect and highlight objects

--- src/overhead_vision/src/main.cpp
+++ src/overhead_vision/src/main.cpp
@@ -45,14 +45,14 @@ int main(int, char**)
     namedWindow("Camera capture");
     namedWindow("Camera detection");
   
-    cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
-    cvCreateTrackbar("HighH", "Control", &iHighH, 179);
+    cvCreateTrackbar("LowH", "Camera detection", &iLowH, 179); //Hue (0 - 179)
+    cvCreateTrackbar("HighH", "Camera detection", &iHighH, 179);
 
-    cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+    cvCreateTrackbar("LowS", "Camera detection", &iLowS, 255); //Saturation (0 - 255)
     cvCreateTrackbar("HighS", "Control", &iHighS, 255);
 
-    cvCreateTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
-    cvCreateTrackbar("HighV", "Control", &iHighV, 255);
+    cvCreateTrackbar("LowV", "Camera detection", &iLowV, 255); //Value (0 - 255)
+    cvCreateTrackbar("HighV", "Camera detection", &iHighV, 255);
 
 
     Mat frame, frame_HSV, frame_threshold,edges;
@@ -68,16 +68,31 @@ int main(int, char**)
 
 
         inRange(frame_HSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), frame_threshold); 
-        imshow("Camera detection", frame_threshold);
-
+        imshow("Camera capture", frame);
+        imshow("Camera detection", frame_HSV);
         GaussianBlur(frame_threshold, edges, Size(9,9), 4.0);
         Canny(edges, edges, 0, 30, 3);
         findContours(edges, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-        vector<vector<Point> > contours_poly( contours.size() );
         
+        vector<vector<Point> > contours_poly( contours.size() );
         vector<Rect> boundRect( contours.size() );
         vector<Point2f>centers( contours.size() );
+
+
+        for( size_t i = 0; i < contours.size(); i++ )
+        {
+            approxPolyDP( contours[i], contours_poly[i], 3, true );
+            boundRect[i] = boundingRect( contours_poly[i] );
+
+        }
         
+        for( size_t i = 0; i< contours.size(); i++ )
+        {
+            Scalar color = Scalar( 0, 255 , 0);
+            //drawContours( drawing, contours_poly, (int)i, color );
+            rectangle( "Camera capture", boundRect[i].tl(), boundRect[i].br(), color, 2 );
+        }
+
         waitKey(1);
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
