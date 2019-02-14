df18f1f3a0b64ec7c6b1ae7242b1bf802ef0fd35
Nlipski <13ml76@queensu.ca>
2019-02-13 21:23:52 -0500

    overhead stuff

--- src/overhead_vision/src/main.cpp
+++ src/overhead_vision/src/main.cpp
@@ -3,6 +3,7 @@
 #include "opencv2/videoio/videoio_c.h"
 #include <math.h> 
 #include <stdio.h>
+#include <libfreenect.h>
 
 using namespace cv;
 using namespace std;
@@ -21,52 +22,60 @@ public:
 int main(int, char**)
 {
     VideoCapture cap(0);
-    cap.set(CV_CAP_PROP_FRAME_WIDTH, 480); 
-    cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480); 
+    cap.set(CV_CAP_PROP_FRAME_WIDTH, 800); 
+    cap.set(CV_CAP_PROP_FRAME_HEIGHT, 600); 
 
     if(!cap.isOpened())  // check if we succeeded
         return -1;
 
     vector<object> object_list;
+
     Mat edges;
 
-    namedWindow("edges",1);
+
+    int iLowH = 0;
+    int iHighH = 179;
+
+    int iLowS = 0; 
+    int iHighS = 255;
+
+    int iLowV = 0;
+    int iHighV = 255;
+
+  
+    cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+    cvCreateTrackbar("HighH", "Control", &iHighH, 179);
+
+    cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+    cvCreateTrackbar("HighS", "Control", &iHighS, 255);
+
+    cvCreateTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
+    cvCreateTrackbar("HighV", "Control", &iHighV, 255);
+
+
+
 
     for(;;)
     {
         Mat frame;
-        Mat thresh;
+        
+
+        Mat imgHSV;
+        Mat imgThresholded;
+
         vector<cv::Vec4i> hierarchy;
         vector<vector<Point> > contours;
         
         cap >> frame; // get a new frame from camera
 
-        Mat drawing = Mat::zeros( frame.size(), CV_8UC3 );
 
-        cvtColor(frame, edges, COLOR_BGR2GRAY);
+        cvtColor(frame, imgHSV, COLOR_BGR2HSV);
+        inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); 
+
         GaussianBlur(edges, edges, Size(9,9), 4.0);
         Canny(edges, edges, 0, 30, 3);
         findContours(edges, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
         vector<vector<Point> > contours_poly( contours.size() );
-        vector<Rect> boundRect( contours.size() );
-        cout<<"num of objects: "<< contours.size()<<endl;
-        for (int i = 0; i < contours.size(); i++) {
-            vector<Point> pts = contours.at(i);
-
-            approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
-            boundRect[i] = boundingRect( Mat(contours_poly[i]) );
-
-
-            cout << "num of vertecies: "<< pts.size()<<endl;
-            //double angle = atan((pts.at(3).y-pts.at(2).y)/(pts.at(3).x-pts.at(2).x));
-            cout<<"Object "<<i<<" has points:"<<endl;
-            for (int j = 0; j < pts.size(); j++)
-                cout<<"Point "<<j<<" is at: "<< pts.at(j)<<endl;
-            drawContours(drawing, contours, i, cv::Scalar(255, 255, 255));
-            rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), cv::Scalar(255, 255, 255), 2, 8, 0 );
-        }
-        
-    
         
         imshow("drawing", drawing);
         waitKey(1);
